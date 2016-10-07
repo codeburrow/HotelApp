da176be24b8c3e3fca39a7d4fb6676567fb76f51
Konstantinos Loutas <k.ls@me.com>
2016-10-07 12:47:02 +0300

    Initial Commit

---
+++ HotelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelApp
+//
+//  Created by Konstantinos Loutas on 10/07/16.
+//  Copyright © 2016 Constantine Lutas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
