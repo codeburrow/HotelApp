8355665675bee086bf3334bb71d414bfdae2c82d
Konstantinos Loutas <k.ls@me.com>
2016-10-07 13:26:41 +0300

    Add functionality to setup new iBeacons

--- HotelApp/ViewController.swift
+++ HotelApp/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var beaconInRangeLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+// MARK: - iBeacon Functionality
+extension ViewController {
+    
+    func setupBeacon(withUuidString uuidString: String) {
+        
+        // Enter Your iBeacon UUID
+        let uuid = UUID(uuidString: uuidString)!
+        
+        // Use identifier like your company name or website
+        let identifier = "com.alphansotech"
+        
+        let Major:CLBeaconMajorValue = 100
+        let Minor:CLBeaconMinorValue = 1
+        
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: Major, minor: Minor, identifier: identifier)
+        
+        // call delegate when Enter iBeacon Range
+        beaconRegion.notifyOnEntry = true
+        
+        // call delegate when Exit iBeacon Range
+        beaconRegion.notifyOnExit = true
+        
+        // Request permission to use location services
+        locationManager.requestAlwaysAuthorization()
+        
+        // Start monitoring the specified iBeacon Region
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.pausesLocationUpdatesAutomatically = false
+        
+    }
+    
 }
 
