96d3bfd0bed9e86bd57df75bb148474fa9822f26
Konstantinos Loutas <k.ls@me.com>
2016-10-10 11:32:12 +0300

    Refactor CLLocationManagerDelegate methods

--- HotelApp/AppDelegate.swift
+++ HotelApp/AppDelegate.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    let locationManager = CLLocationManager()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        
+        // Request permission to use location services
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedAlways) {
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -41,6 +49,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
 
+// MARK: - CLLocationManagerDelegate
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+            
+        case .authorizedAlways, .authorizedWhenInUse:
+            // Starts the generation of updates that report the user’s current location.
+            simpleAlert(title: "Authorised", message: "\(status) authorisation granted")
+            locationManager.startUpdatingLocation()
+            
+        case .restricted:
+            // Your app is not authorized to use location services.
+            simpleAlert(title: "Permission Error", message: "Need Location Service Permission To Access Beacon")
+            
+            
+        case .denied:
+            // The user explicitly denied the use of location services for this app or location services are currently disabled in Settings.
+            simpleAlert(title: "Permission Error", message: "Need Location Service Permission To Access Beacon")
+            
+        default:
+            // handle .NotDetermined here
+            // The user has not yet made a choice regarding whether this app can use location services.
+            break
+        }
+    }
+    
+}
+
+// MARK: - Simple alerting
+extension AppDelegate {
+    func simpleAlert(title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+        print("\(title): \(message)")
+        self.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+}
--- HotelApp/ViewController.swift
+++ HotelApp/ViewController.swift
@@ -22,18 +22,8 @@ class ViewController: UIViewController {
         // Set ourselves as the CLLocationManager delegate for our locationManager
         locationManager.delegate = self
         
-        // Request permission to use location services
-        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedAlways) {
-            locationManager.requestAlwaysAuthorization()
-        }
-        
         // Setup our fake iPad beacon
         setupBeacon(withUuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
@@ -46,9 +36,8 @@ extension ViewController {
         // Enter Your iBeacon UUID
         let uuid = UUID(uuidString: uuidString)!
         
-        // Use identifier like your company name or website
+        // Setup beacon details
         let identifier = "com.CLutas.fakeBeacon.iPadAir"
-        
         let Major:CLBeaconMajorValue = 0
         let Minor:CLBeaconMinorValue = 0
         
@@ -70,41 +59,9 @@ extension ViewController {
     
 }
 
-// MARK: - Simple alerting
-extension ViewController {
-    func simpleAlert(title:String, message:String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
-        self.present(alertController, animated: true, completion: nil)
-    }
-}
 
 // MARK: - CLLocationManagerDelegate
 extension ViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        
-        switch status {
-            
-        case .authorizedAlways, .authorizedWhenInUse:
-            // Starts the generation of updates that report the user’s current location.
-            locationManager.startUpdatingLocation()
-            
-        case .restricted:
-            // Your app is not authorized to use location services.
-            simpleAlert(title: "Permission Error", message: "Need Location Service Permission To Access Beacon")
-            
-            
-        case .denied:
-            // The user explicitly denied the use of location services for this app or location services are currently disabled in Settings.
-            simpleAlert(title: "Permission Error", message: "Need Location Service Permission To Access Beacon")
-            
-        default:
-            // handle .NotDetermined here            
-            // The user has not yet made a choice regarding whether this app can use location services.
-            break
-        }
-    }
     
     func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
         // Tells the delegate that a iBeacon Area is being monitored
@@ -132,6 +89,7 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    // MARK: Beacon monitoring
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         print("didEnterRegion with identifier \(region.identifier)")
     }
@@ -140,6 +98,7 @@ extension ViewController: CLLocationManagerDelegate {
         print("didExitRegion with identifier \(region.identifier)")
     }
     
+    // MARK: Beacon ranging
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if !beacons.isEmpty {
             let currentDateTime = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .medium)
@@ -148,6 +107,7 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
+    // Error handling
     func locationManager(_ manager: CLLocationManager, rangingBeaconsDidFailFor region: CLBeaconRegion, withError error: Error) {
         print("Ranging failed with error \(error.localizedDescription)")
     }
