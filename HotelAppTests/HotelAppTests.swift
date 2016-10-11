292d5b097c22d12e641d46b20252daa8907ddacb
Konstantinos Loutas <k.ls@me.com>
2016-10-11 11:32:28 +0300

    Add console logging to remote notification request unit test

--- HotelAppTests/HotelAppTests.swift
+++ HotelAppTests/HotelAppTests.swift
@@ -38,6 +38,10 @@ class HotelAppTests: XCTestCase {
         let expect = expectation(description: "waitForWebService")
         let notifRequestUrl = "https://hotelapp-web.herokuapp.com/push"
         Alamofire.request(notifRequestUrl).responseJSON { (response) in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
             }
