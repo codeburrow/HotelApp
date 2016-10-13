afdc3dc6e82178508d3912fa7f64d1d4ea836f36
Konstantinos Loutas <k.ls@me.com>
2016-10-13 13:01:56 +0300

    Add test for server device token registration

--- HotelAppTests/HotelAppTests.swift
+++ HotelAppTests/HotelAppTests.swift
@@ -45,4 +45,28 @@ class HotelAppTests: XCTestCase {
         XCTAssertEqual(serverResponseString!, "Successful Delivery!\n")
     }
     
+    func testDeviceTokenRegistration() {
+        //let deviceToken = "052ECE50BFD2D03315A4835D3AEB156C0B64BB879F75063FA20CEAF1F54DEE31"
+        let userID = "2"
+        let parameters: Parameters = ["user_id": userID]
+        
+        let expect = expectation(description: "waitForServerResponse")
+        var serverResponseString: String?
+        
+        Alamofire.request("http://hotelapp-web.herokuapp.com/getUserIdFromPostRequest", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseString { (response) in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            if let responseValue = response.result.value {
+                print("Response value: \(responseValue)")
+                serverResponseString = responseValue
+            }
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+        XCTAssertFalse((serverResponseString?.isEmpty)!)
+    }
+    
 }
